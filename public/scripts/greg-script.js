c3f88fbdd3630d44d6deae52854244432ae6c08f
eSsXiV <essxiv@gmail.com>
2018-02-02 12:19:35 -0800

    contributor

--- public/scripts/greg-script.js
+++ public/scripts/greg-script.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     if (location.pathname === "/mdinvest/" || location.pathname === "/mdinvest") {
         $('.learn-link').addClass('invest-selected');
     } else if (location.pathname === "/mdinvest/register" || location.pathname === "/mdinvest/register/") {
@@ -6,6 +7,16 @@ $(document).ready(function() {
         $('.sub-link').addClass('invest-selected');
     } else {
         $('.doc-link').addClass('invest-selected');
-        
     }
+
+    if (location.pathname === "/mdcontributor/" || location.pathname === "/mdcontributor") {
+        $('.learn-link').addClass('invest-selected');
+    } else if (location.pathname === "/mdcontributor/register" || location.pathname === "/mdcontributor/register/") {
+        
+        $('.sub-link').addClass('invest-selected');
+    } else {
+        $('.doc-link').addClass('invest-selected');
+    }
+
+
 })
